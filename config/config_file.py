817339e3dfec32a1af7344e08f01fc32b06b9118
Jeovani Marcos Morales Nieto <jeovani.morales@academicos.udg.mx>
2021-05-04 00:27:20 -0500

    Update config_file.py
    Add Token

--- config/config_file.py
+++ config/config_file.py
@@ -1,6 +1,7 @@
 #===========================================
 # Data to modify
 #=========================================
+TOKEN = "replace TOKEN" # Paste the TOKEN provided by Botfather
 
 subject_data = {
   "_id": "replace subject_id", # Database name
