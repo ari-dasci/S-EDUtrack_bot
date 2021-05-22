1fa2b7e9d6cfdaa6799fa670d9ff20c1fe65c7dd
Jeovani Marcos Morales Nieto <jeovani.morales@academicos.udg.mx>
2021-05-22 14:40:43 -0500

    Update edutrack.py
    Fix for error of PORT or bad webhook 0.0.0.0.0 is reserved on Heroku

--- edutrack.py
+++ edutrack.py
@@ -56,46 +56,6 @@ else:
     sys.exit(logging.info(msg_log))
   mode = "dev"
 
-# if mode == "dev":
-
-#   def run(updater):
-#     updater.start_polling()
-#     msg_log = """
-#     ====================================================
-#     | EDUtrack_bot  Copyright (C) 2021  Jeovani Morales
-#     | This program comes with ABSOLUTELY NO WARRANTY.
-#     | This is free software, and you are welcome
-#     | to redistribute it under certain conditions see
-#     | https://github.com/jeovani-morales/EDUtrack_bot
-#     ====================================================
-
-#     Loaded Bot"""
-#     logging.info(msg_log)
-#     print(msg_log)
-#     updater.idle()  # Permite finalizar el bot con Ctrl + C
-
-
-# elif mode == "prod":
-
-#   def run(updater):
-#     PORT = int(os.environ.get("PORT", "8443"))
-#     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
-#     updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-#     updater.bot.set_webhook(f"https://{HEROKU_APP_NAME}.herokuapp.com/{TOKEN}")
-#     msg_log = """
-#     ====================================================
-#     | EDUtrack_bot  Copyright (C) 2021  Jeovani Morales
-#     | This program comes with ABSOLUTELY NO WARRANTY.
-#     | This is free software, and you are welcome
-#     | to redistribute it under certain conditions see
-#     | https://github.com/jeovani-morales/EDUtrack_bot
-#     ====================================================
-
-#     Loaded Bot"""
-#     logging.info(msg_log)
-#     print(msg_log)
-
-
 def main():
   try:
     # Bot's conecction
@@ -153,8 +113,7 @@ def main():
     dp.add_handler(Cmd_Hdl("check_email", cmd.check_email))
     dp.add_handler(Cmd_Hdl("suggestion", cmd.suggestion))
 
-
-
+    # Start to receive updates
     if mode == "dev":
       updater.start_polling()
       msg_log = """
@@ -172,10 +131,12 @@ def main():
       updater.idle()  # Permite finalizar el bot con Ctrl + C
 
     elif mode == "prod":
-      PORT = int(os.environ.get("PORT", 5000))
+      PORT = int(os.environ.get("PORT", 8443))
       HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
-      updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-      updater.bot.set_webhook(f"https://{HEROKU_APP_NAME}.herokuapp.com/{TOKEN}")
+      updater.start_webhook(listen="0.0.0.0",
+                      port=int(PORT),
+                      url_path=TOKEN,
+                      webhook_url=f"https://{HEROKU_APP_NAME}.herokuapp.com/{TOKEN}")
       msg_log = """
       ====================================================
       | EDUtrack_bot  Copyright (C) 2021  Jeovani Morales
@@ -189,12 +150,6 @@ def main():
       logging.info(msg_log)
       print(msg_log)
 
-
-
-
-
-    # Start to receive updates
-    #run(updater)
   except:
     error_path = f"{inspect.stack()[0][1]} - {inspect.stack()[0][3]}"
     g_fun.print_except(error_path)
